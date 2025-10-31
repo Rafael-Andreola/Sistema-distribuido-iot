bbb01e31d843f7bdfd46a0156d6320f2b3845767
Rafael Andreola <103367273+Rafael-Andreola@users.noreply.github.com>
2025-10-30 23:29:31 -0300

    Add docker compose and update main

--- src/main.c
+++ src/main.c
@@ -8,8 +8,6 @@
 #include "task.h"
 #include "queue.h"
 
-#define SENSOR_INTERVAL_MS 2000
-
 QueueHandle_t mqttQueue;
 
 // Estrutura de mensagem
@@ -18,12 +16,12 @@ typedef struct {
     char payload[128];
 } MqttMessage;
 
-// Simula leitura de um sensor (ex: temperatura)
+// Função simulada de leitura do sensor
 float read_sensor() {
     return (rand() % 3000) / 100.0f;
 }
 
-// ===================== TAREFA DO SENSOR =====================
+// ===================== TAREFA SENSOR =====================
 void vSensorTask(void *pvParameters) {
     printf("[SensorTask] Iniciada.\n");
     fflush(stdout);
@@ -32,43 +30,28 @@ void vSensorTask(void *pvParameters) {
         float value = read_sensor();
         MqttMessage msg;
 
+        const char *sensor_interval = getenv("SENSOR_INTERVAL_MS");
+
         snprintf(msg.topic, sizeof(msg.topic), "v1/devices/me/telemetry");
         snprintf(msg.payload, sizeof(msg.payload),
                  "{\"temperature\": %.2f}", value);
 
         if (xQueueSend(mqttQueue, &msg, portMAX_DELAY) == pdPASS) {
             printf("[SensorTask] Valor lido: %.2f\n", value);
-            fflush(stdout);
         } else {
             printf("[SensorTask] Erro ao enviar para fila MQTT!\n");
-            fflush(stdout);
         }
-
-        vTaskDelay(pdMS_TO_TICKS(SENSOR_INTERVAL_MS));
+        
+        fflush(stdout);
+        vTaskDelay(pdMS_TO_TICKS(sensor_interval ? atoi(sensor_interval) : 2000));
     }
-}
-
-// ===================== TAREFA MQTT =====================
-static void on_connect(struct mosquitto *mosq, void *obj, int rc)
-{
-    int *connected = (int *)obj;
-    if (rc == 0) {
-        *connected = 1;
-        printf("[MQTT] on_connect: conectado com sucesso.\n");
-    } else {
-        *connected = 0;
-        printf("[MQTT] on_connect: falha (%s)\n", mosquitto_strerror(rc));
-    }
-    fflush(stdout);
 }
 
 int connect_broker(struct mosquitto *mosq, const char *host, int port) 
 {
     // Implementar conexão com o broker MQTT
-    int rc;
-
     printf("[MQTT] Conectando ao broker %s:%d...\n", host, port);
-    rc = mosquitto_connect(mosq, host, port,  60);
+    int rc = mosquitto_connect(mosq, host, port,  60);
 
     if (rc == MOSQ_ERR_INVAL)
     {
@@ -85,70 +68,16 @@ int connect_broker(struct mosquitto *mosq, const char *host, int port)
     return rc;
 }
 
-void vMqttTask(void *pvParameters) {
-    printf("[MqttTask] Iniciada.\n");
-    fflush(stdout);
-
-    struct mosquitto *mosq = NULL;
+void sendMessages(struct mosquitto *mosq) {
+    // Implementar envio de mensagens MQTT
     MqttMessage msg;
-
-    const char *token = getenv("TAGO_DEVICE_TOKEN");
-    const char *client_id = getenv("TAGO_CLIENT_ID");  // opcional
-    const char *freertos_mqtt_host = getenv("freertos_mqtt_host");  // opcional
-    const char *freertos_mqtt_port = getenv("freertos_mqtt_port");  // opcional
-
-    if (!token) {
-        printf("[ERRO] Variável de ambiente TAGO_DEVICE_TOKEN não definida!\n");
-        vTaskDelete(NULL);
-    }
-    else {
-        printf("[MqttTask] Usando TAGO_DEVICE_TOKEN: %s\n", token);
-    }
-
     
-    mosquitto_lib_init();
-    mosq = mosquitto_new(client_id, true, NULL);
-    
-    // Nenhum usuário/senha necessário — conexão local
-    mosquitto_username_pw_set(mosq, token, "");
-    int rc = connect_broker(mosq, freertos_mqtt_host, atoi(freertos_mqtt_port));
-
-    printf("[MQTT] mosquitto_connect iniciado com sucesso.\n");
-
-    fflush(stdout);
-
-    /* Inicia thread interna da libmosquitto para gerenciar I/O e reconexões */
-    if (mosquitto_loop_start(mosq) != MOSQ_ERR_SUCCESS) {
-        printf("[ERRO] Falha ao iniciar loop do mosquitto.\n");
-        mosquitto_disconnect(mosq);
-        mosquitto_destroy(mosq);
-        mosquitto_lib_cleanup();
-        vTaskDelete(NULL);
-        return;
-    }
-
-    /* Espera até o callback confirmar a conexão */
-    // int waitCount = 0;
-    // while (!connected && waitCount++ < 100) { // timeout ~100*100ms = 10s
-    //     vTaskDelay(pdMS_TO_TICKS(100));
-    // }
-
-    // if (!connected) {
-    //     printf("[ERRO] Timeout aguardando conexão MQTT. Continuando — loop do mosquitto tentará reconectar.\n");
-    // } else {
-    //     printf("[MQTT] Conectado ao broker %s:%d\n", TAGO_BROKER, TAGO_PORT);
-    // }
-
-    fflush(stdout);
-
-    /* Loop de envio de mensagens */
     while (1) {
         if (xQueueReceive(mqttQueue, &msg, portMAX_DELAY)) {
-
             int mid = 0;
 
-            rc = mosquitto_publish(mosq, &mid, msg.topic, (int)strlen(msg.payload),
-                                   msg.payload, 0, false);
+            int rc = mosquitto_publish(mosq, &mid, msg.topic, (int)strlen(msg.payload),
+                                   msg.payload, 1, false);
 
             if(mid == 0) {
                 printf("[ERRO] Falha ao obter ID da mensagem publicada em %s.\n", msg.topic);
@@ -162,11 +91,58 @@ void vMqttTask(void *pvParameters) {
             } else {
                 printf("[ERRO] Falha ao publicar em %s: %s\n", msg.topic, mosquitto_strerror(rc));
             }
+
             fflush(stdout);
         }
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
+}
+
+void vMqttTask(void *pvParameters) {
+    printf("[MqttTask] Iniciada.\n");
+    fflush(stdout);
+
+    struct mosquitto *mosq = NULL;
+
+    const char *token = getenv("DEVICE_TOKEN");
+    const char *client_id = getenv("CLIENT_ID");
+    const char *mqtt_host = getenv("MQTT_HOST");
+    const char *mqtt_port = getenv("MQTT_PORT");
+
+    if (!token) {
+        printf("[ERRO] Variável de ambiente DEVICE_TOKEN não definida!\n");
+        vTaskDelete(NULL);
+    }
+
+    printf("[MqttTask] Usando DEVICE_TOKEN: %s\n", token);
+
+    mosquitto_lib_init();
+    mosq = mosquitto_new(client_id, true, NULL);
+
+    if (!mosq) {
+        printf("[ERRO] Falha ao criar instância do mosquitto.\n");
+        mosquitto_lib_cleanup();
+        vTaskDelete(NULL);
+    }
+
+    mosquitto_username_pw_set(mosq, token, "");
+    int rc = connect_broker(mosq, mqtt_host, atoi(mqtt_port));
+
+    printf("[MQTT] mosquitto_connect iniciado com sucesso.\n");
+    fflush(stdout);
+
+    /* Inicia thread interna da libmosquitto para gerenciar I/O e reconexões */
+    if (mosquitto_loop_start(mosq) != MOSQ_ERR_SUCCESS) {
+        printf("[ERRO] Falha ao iniciar loop do mosquitto.\n");
+        mosquitto_disconnect(mosq);
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+        vTaskDelete(NULL);
+        return;
+    }
+
+    sendMessages(mosq);
 
     mosquitto_loop_stop(mosq, true);
     mosquitto_disconnect(mosq);
@@ -175,30 +151,43 @@ void vMqttTask(void *pvParameters) {
     vTaskDelete(NULL);
 }
 
+// Função para criar a fila MQTT
+void create_mqtt_queue() {
+
+    const char *queue_size = getenv("QUEUE_SIZE");
+
+    mqttQueue = xQueueCreate(atoi(queue_size), sizeof(MqttMessage));
+
+    if (mqttQueue == NULL) {
+        printf("[ERRO] Falha ao criar fila MQTT.\n");
+        exit(1);
+    }
+}
+
 // ===================== MAIN =====================
 int main(void) {
     printf("Iniciando FreeRTOS MQTT TagoIO Simulation...\n");
     fflush(stdout);
 
-    mqttQueue = xQueueCreate(10, sizeof(MqttMessage));
-    if (mqttQueue == NULL) {
-        printf("[ERRO] Falha ao criar fila MQTT.\n");
+    create_mqtt_queue();
+
+    if (xTaskCreate(vSensorTask, "SensorTask", 1024, NULL, 1, NULL) == pdPASS)
+        printf("[OK] SensorTask criada.\n");
+    else
+    {
+        printf("[ERRO] Falha ao criar SensorTask!\n");
         return 1;
     }
 
-    if (xTaskCreate(vSensorTask, "SensorTask", 1024, NULL, 1, NULL) != pdPASS)
-        printf("[ERRO] Falha ao criar SensorTask!\n");
-    else
-        printf("[OK] SensorTask criada.\n");
-
-    if (xTaskCreate(vMqttTask, "MqttTask", 2048, NULL, 1, NULL) != pdPASS)
-        printf("[ERRO] Falha ao criar MqttTask!\n");
-    else
+    if (xTaskCreate(vMqttTask, "MqttTask", 2048, NULL, 1, NULL) == pdPASS)
         printf("[OK] MqttTask criada.\n");
+    else
+    {
+        printf("[ERRO] Falha ao criar MqttTask!\n");
+        return 1;
+    }
 
     vTaskStartScheduler();
 
-    // Nunca deve chegar aqui
-    for (;;);
     return 0;
 }
